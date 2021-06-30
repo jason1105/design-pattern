5bfe7e8cb95fd6072c2194297bc90bd4406b1d51
lv-wei <13366824@qq.com>
2021-06-30 21:35:51 +0800

    builder

--- src/main/java/com/lvw/designpattern/gof/client/MazeGame.java
+++ src/main/java/com/lvw/designpattern/gof/client/MazeGame.java
@@ -2,10 +2,7 @@ package com.lvw.designpattern.gof.client;
 
 import com.lvw.designpattern.gof.maze.*;
 import com.lvw.designpattern.gof.maze.bombed.BombedMazeFactory;
-import com.lvw.designpattern.gof.maze.default_.DefaultMazeFactory;
-import com.lvw.designpattern.gof.maze.default_.Door;
-import com.lvw.designpattern.gof.maze.default_.Maze;
-import com.lvw.designpattern.gof.maze.default_.Room;
+import com.lvw.designpattern.gof.maze.default_.*;
 
 // Client source.
 public class MazeGame {
@@ -14,35 +11,18 @@ public class MazeGame {
 
         // use abstract factory to create various game of type
         MazeGame game = new MazeGame();
-        Maze maze = game.CreateMaze(new DefaultMazeFactory());
+        Maze maze = game.CreateMaze(new DefaultMazeBuilder());
         System.out.println(maze);
 
     }
 
-    public Maze CreateMaze(MazeFactory factory) {
+    public Maze CreateMaze(MazeBuilder builder) { // use interface instead of concrete class
 
-        /*
-        Use simple factory instead of 'new'.
-        */
-        Maze maze = factory.MakeMaze();
+        builder.BuildMaze();
+        builder.BuildRoom(1);
+        builder.BuildRoom(2);
+        builder.BuildDoor(1,2);
 
-        Room r1 = factory.MakeRoom(1);
-        Room r2 = factory.MakeRoom(2);
-        Door door = factory.MakeDoor(r1, r2);
-
-        r1.SetSide(Direction.East, door);
-        r1.SetSide(Direction.West, factory.MakeWall());
-        r1.SetSide(Direction.South, factory.MakeWall());
-        r1.SetSide(Direction.North, factory.MakeWall());
-
-        r2.SetSide(Direction.East, factory.MakeWall());
-        r2.SetSide(Direction.West, door);
-        r2.SetSide(Direction.South, factory.MakeWall());
-        r2.SetSide(Direction.North, factory.MakeWall());
-
-        maze.AddRoom(r1);
-        maze.AddRoom(r2);
-
-        return maze;
+        return builder.getMaze();
     }
 }
--- src/main/java/com/lvw/designpattern/gof/client/MazeGameBomb.java
+++ src/main/java/com/lvw/designpattern/gof/client/MazeGameBomb.java
@@ -1,7 +1,9 @@
 package com.lvw.designpattern.gof.client;
 
 import com.lvw.designpattern.gof.maze.Direction;
+import com.lvw.designpattern.gof.maze.MazeBuilder;
 import com.lvw.designpattern.gof.maze.MazeFactory;
+import com.lvw.designpattern.gof.maze.bombed.BombedMazeBuilder;
 import com.lvw.designpattern.gof.maze.bombed.BombedMazeFactory;
 import com.lvw.designpattern.gof.maze.bombed.BombedWall;
 import com.lvw.designpattern.gof.maze.bombed.RoomWithABomb;
@@ -16,16 +18,26 @@ public class MazeGameBomb<R> {
 
         // use abstract factory to create various game of type
         MazeGameBomb<Room> bombGame = new MazeGameBomb();
-        Maze maze = bombGame.CreateMaze(new BombedMazeFactory());
+        Maze maze = bombGame.CreateMaze(new BombedMazeBuilder()); // use builder
         System.out.println(maze);
 
     }
 
-    public Maze CreateMaze(MazeFactory factory) {
+    public Maze CreateMaze(MazeBuilder builder) { // use interface instead of concrete class
 
-        /*
+        builder.BuildMaze();
+        builder.BuildRoom(1);
+        builder.BuildRoom(2);
+        builder.BuildDoor(1,2);
+
+        return builder.getMaze();
+    }
+
+    /*public Maze CreateMaze(MazeFactory factory) {
+
+        *//*
         Use simple factory instead of 'new'.
-        */
+        *//*
         Maze maze = factory.MakeMaze();
 
         RoomWithABomb r1 = (RoomWithABomb)factory.MakeRoom(1);
@@ -49,5 +61,5 @@ public class MazeGameBomb<R> {
         System.out.println(((BombedWall) r1.GetSide(Direction.North)).getBomb());
 
         return maze;
-    }
+    }*/
 }
