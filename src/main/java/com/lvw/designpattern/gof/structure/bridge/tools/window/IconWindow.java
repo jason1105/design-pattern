2aac551ce49b613b210232659f791c6653fe8f21
lv-wei <13366824@qq.com>
2022-12-08 23:37:15 +0800

    Add some classes

--- src/main/java/com/lvw/designpattern/gof/structure/bridge/tools/window/IconWindow.java
+++ src/main/java/com/lvw/designpattern/gof/structure/bridge/tools/window/IconWindow.java
@@ -1,6 +1,5 @@
 package com.lvw.designpattern.gof.structure.bridge.tools.window;
 
-import com.lvw.designpattern.gof.structure.bridge.tools.PixPoint;
 import com.lvw.designpattern.gof.structure.bridge.tools.Point;
 import com.lvw.designpattern.gof.structure.bridge.tools.Window;
 
@@ -12,5 +11,9 @@ public class IconWindow implements Window {
     @Override
     public void drawWindow(Point p0, Point p1) {
         // 绘制没有边线的窗口
+    }
+
+    public void drawPix(Point p) {
+        
     }
 }
